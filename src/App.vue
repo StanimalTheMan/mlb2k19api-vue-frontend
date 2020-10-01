cd346bc2e823ae840e60aa1e33cec925f12fd3f5
StanimalTheMan <sjc591@nyu.edu>
2020-10-01 11:23:52 -0400

    added some background theme and changed title of app

--- src/App.vue
+++ src/App.vue
@@ -17,5 +17,28 @@ export default {
 </script>
 
 <style>
+body {
+  margin: 0;
+  padding: 0;
+  font-family: sans-serif;
+  background: linear-gradient(
+    90deg,
+    rgba(36, 35, 0, 1) 0%,
+    rgba(121, 9, 12, 1) 0%,
+    rgba(0, 53, 255, 1) 100%
+  );
+}
 
+a {
+  color: white;
+}
+
+#links {
+  text-align: center;
+}
+ul {
+  display: inline-block;
+  text-align: left;
+  font: 24px "Times New Roman", san-serif;
+}
 </style>
